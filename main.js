59ce787c29f1335b0df761f0d73be65cc1c49fd2
guti <lucas.gutierres.gomes@gmail.com>
2023-02-17 15:55:25 -0300

    added variables for discounting

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var premioDisplay = document.getElementById('premio')
+var descontoDisplay = document.getElementById('desconto')
+var premiondDisplay = document.getElementById('premio_nd')
 var coberturas = [
 	document.getElementById('cobertura1'),
 	document.getElementById('cobertura2'),
@@ -17,8 +19,12 @@ function calculo_p() {
 	return p
 }
 
-var p_value = 10+calculo_p();
+var d_value = 2;
+var p_value_crude = 10+calculo_p();
+var p_value = p_value_crude-d_value;
 
 premioDisplay.textContent = "R$ " + String(p_value.toFixed(2));
+descontoDisplay.textContent = "Seu desconto é de R$ " + d_value;
+premiondDisplay.textContent = "O valor original é de R$" + p_value_crude
 
 //console.log(premioDisplay.textContent);
