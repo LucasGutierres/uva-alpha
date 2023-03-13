763a6767edc2b015177f4f6f5d8654ef51e3c5fb
Lucas Gutierres Gomes <lucas.gutierres.gomes@gmail.com>
2023-03-12 21:22:18 -0300

    each cobertura's premio and desconto are calculate individually

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ var premioDisplay = document.getElementById('premio')
 var descontoDisplay = document.getElementById('desconto')
 var premiondDisplay = document.getElementById('premio_nd')
 var coberturas = []
+var coberturas_l = []
 var sensores = []
+var desconto_mapa = [[1,1,1,0,0,0,0],[1,1,1,0,1,0,1],[1,1,1,1,1,1,1]]
 
 var v_ind = document.getElementById('v_indenização')
 
@@ -18,48 +20,70 @@ function get_options(id, name) {
 	return c;
 }
 
-function calculo_d() {
+function build_coberturas(mapa) {
+	for (let i = 0; i<mapa.length; i++){
+		if (mapa[i] == 1){
+			coberturas_l[i].style.fontWeight = 'bold'
+		}
+		else {
+			coberturas_l[i].style.fontWeight = 'normal'
+		}
+	}
+}
+
+function calculo() {
+	let v = []
 	let d = 0;
+	let p = 0;
+	let pd = 0;
 	for (let i = 0; i<sensores.length; i++){
 		if (sensores[i].checked) {
+			build_coberturas(desconto_mapa[i])
 			d = sensores[i].value
 		}
 	}
-	return d
-}
-
-function calculo_p() {
-	let p = 0;
-	for (let i = 0; i<coberturas.length; i++) {
+	for (let i = 0; i<coberturas.length; i++){
 		if (coberturas[i].checked) {
-			p += Number(coberturas[i].value)
+			if (coberturas_l[i].style.fontWeight == 'normal') {
+				p += Number(coberturas[i].value)
+			}
+			else if (coberturas_l[i].style.fontWeight == 'bold') {
+				pd += Number(coberturas[i].value)
+			}
 		}
 	}
-	return p
+	v = [d,p,pd]
+	return v
 }
 
 function set_options() {
 	coberturas = get_options('sel_coberturas', 'INPUT');
+	coberturas_l = get_options('sel_coberturas', 'LABEL');
 	sensores = get_options('sel_sensores', 'INPUT')
 }
 
 function new_p() {
 	set_options();
-	let d_value = calculo_d();
-	let p_value_crude = calculo_p();
-	let p_value = p_value_crude*(100-d_value)/100;
+	let valores = calculo();
+	let d_value = valores[0];
+	let p_value = valores[1];
+	let pd_value = valores[2];
+	let p_crude = p_value+pd_value;
+	let pd_m = ((pd_value*(100-d_value))/100);
+	let p_m = (p_value+pd_m);
+	let p_a = (p_m)*12;
 
-	premioDisplay.textContent = "R$ " + p_value.toFixed(2);
-	descontoDisplay.textContent = "Seu desconto é de " + d_value + "%"
-	premiondDisplay.textContent = "O valor original é de R$" + p_value_crude
+	premioDisplay.textContent = "R$ " + p_a.toFixed(2);
+	descontoDisplay.textContent = "Seu desconto é de R$ " + ((p_crude-p_m)*12).toFixed(2) + " ("+ d_value + "%)"
+	premiondDisplay.textContent = "O valor original é de R$" + (p_crude*12)
 }
 
 document.body.addEventListener('change', new_p);
 
-v_ind.oninput = function(){
+new_p();
+
+/*v_ind.oninput = function(){
 	let c = v_ind.value*10000
 	let cd = String(c).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 	document.getElementById('v_indenização_display').textContent = "Indenização de R$ " + cd + ".00"
-}
-
-new_p();
+}*/
